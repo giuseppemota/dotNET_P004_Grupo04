f38c262535296aeb0f6b320875aaffce39e3a8ae
Rafaela Brito <rcfbritto@gmail.com>
2023-12-07 15:29:24 -0300

    Adicionando Plano de consultoria

---
+++ PlanoDeConsultoria.cs
@@ -0,0 +1,25 @@
+namespace P004;
+public class PlanoDeConsultoria
+{
+    string? titulo;
+    double mensalidade;
+    public List<string> Beneficios { get; set; }
+
+    public string? Titulo
+    {
+        get { return titulo; }
+        set { titulo = value; }
+    }
+
+    public double Mensalidade
+    {
+        get { return mensalidade; }
+        set { mensalidade = value; }
+    }
+    public PlanoDeConsultoria(string titulo, double _mensalidade, List<string> beneficios)
+    {
+        Titulo = titulo;
+        mensalidade = _mensalidade;
+        Beneficios = beneficios;
+    }
+}
