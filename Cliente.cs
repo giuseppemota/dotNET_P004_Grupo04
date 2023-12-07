3d92b5a4812aa148db5f71bdaf6e64bc912c9539
sergiorock111 <sssantos.ep@uesc.br>
2023-12-07 15:51:14 -0300

    atualização Classe Cliente

--- Cliente.cs
+++ Cliente.cs
@@ -1,7 +1,52 @@
 ﻿namespace P004;
 
 public class Cliente : Pessoa
-{
-    public string? EstadoCivil { get; set; }
-    public string? Profissao { get; set; }
+    {
+        
+    public string? Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string? CPF { get; set; }
+
+    public int Idade
+    {
+        get
+        {
+            return (int)((DateTime.Now - DataNascimento).TotalDays / 365.25);
+        }
+    }
+        PlanoDeConsultoria? planoDeConsultoria;
+
+        public List<IPagamento> pagamentos = new List<IPagamento>();
+
+        public PlanoDeConsultoria planoDeConsultoria
+        {
+            get { return planoDeConsultoria; }
+            set { this.planoDeConsultoria = value; }
+        }
+
+        public List<IPagamento> Pagamentos
+        {
+            get { return pagamentos; }
+            set
+            {
+                foreach (IPagamento pagamento in value)
+                {
+                    this.pagamentos.Add(pagamento);
+                }
+            }
+        }
+
+        public void efetuarPagamento(IPagamento pagamento)
+        {
+            pagamento.RealizarPagamento(this.planoDeSaude!.Mensalidade);
+            this.pagamentos.Add(pagamento);
+        }
+
+        public Paciente(string _nome, DateTime _dataDeNascimento, string _cpf, string _sexo, List<string> _sintomas, PlanoDeSaude? _plano) : base(_nome, _dataDeNascimento, _cpf)
+        {
+            sexo = _sexo;
+            sintomas = _sintomas;
+            PlanoDeSaude = _plano!;
+        }
+    }
 }
